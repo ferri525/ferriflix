dcc733bf5b67535add6dbe15a5cb49c3583ee79a
ferri525 <ferrialisson2@gmail.com>
2020-07-28 16:24:36 -0300

    Modificação Segunda Aula

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,15 +3,16 @@ import Logo from '../../assets/img/logo.png';
 import './menu.css';
 //import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
+import {Link} from 'react-router-dom';
 
 function Menu () {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo"src={Logo} alt="FerriFlix logo"/>
-            </a>
+            </Link>
 
-            <Button as="a"className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
